e5ffebe2d10b3fcd636529aafeef36bd150c6380
qinchen <xiezherui@163.com>
2025-05-17 16:56:33 +0800

    Unx星球后端代码

---
+++ src/main/java/com/qinchen/usercenter/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.qinchen.usercenter.mapper;
+
+import com.qinchen.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ * @author 
+ * @from 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
---
+++ src/test/java/com/qinchen/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,32 @@
+package com.qinchen.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
+
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 启动类测试
+ *
+ * @author 
+ * @from 
+ */
+@SpringBootTest
+class UserCenterApplicationTests {
+
+
+    @Test
+    void testDigest() throws NoSuchAlgorithmException {
+        String newPassword = DigestUtils.md5DigestAsHex(("abcd" + "mypassword").getBytes());
+        System.out.println(newPassword);
+    }
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
+
